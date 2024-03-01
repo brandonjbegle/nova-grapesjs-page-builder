161cde43158e8c04eaa9757c597feb6cca85f195
Brandon Begle <brandon@begleweyer.com>
2024-03-01 13:30:48 -0500

    Rebuild with asset manager

--- routes/inertia.php
+++ routes/inertia.php
@@ -5,6 +5,9 @@
 
     Route::get('/{id}', function (NovaRequest $request, $id) {
         // Todo Brandon: Need to get these dynamically later
+        // Todo Brandon: Will need a migration like nova_grapes_assets that has the path, height, width, name, etc.
+        // Todo Brandon: Will also need a model
+
         $assetManagerAssets = [
             [
                 'src'    => 'http://placehold.it/350x250/79c267/fff/image3.jpg',
