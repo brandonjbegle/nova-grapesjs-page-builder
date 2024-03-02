c4a5278628d1ed8a525b49a4eca2393d8e837ee4
Brandon Begle <brandon@begleweyer.com>
2024-03-01 22:45:21 -0500

    Add url attribute

--- src/Models/NovaGrapesjsAsset.php
+++ src/Models/NovaGrapesjsAsset.php
@@ -3,6 +3,7 @@
     namespace BrandonJBegle\NovaGrapesjsPageBuilder\Models;
 
     use Illuminate\Database\Eloquent\Model;
+    use Illuminate\Support\Facades\Storage;
 
     class NovaGrapesjsAsset extends Model
     {
@@ -13,6 +14,6 @@
         ];
 
         public function getUrlAttribute(){
-
+            return Storage::disk($this->disk)->url($this->path);
         }
   }
